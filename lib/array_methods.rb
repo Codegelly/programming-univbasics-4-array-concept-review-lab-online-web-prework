81024094a82adc4ac33c61614222173f9be72461
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-12 15:21:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,5 +27,16 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  result = nil
+  array.each {|i|
+    if result == nil
+      result = i
+    end
+    
+    if result > i
+      result = i
+    end
+  }
+  
+  return result
 end
